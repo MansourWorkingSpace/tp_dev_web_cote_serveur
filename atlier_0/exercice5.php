dd5096f567e18c9f9670cf59fb3776c67dc2a042
MansourWorkingSpace <mansour.tech.contact@gmail.com>
2024-09-16 19:17:30 +0100

    avec while

--- atlier_0/exercice5.php
+++ atlier_0/exercice5.php
@@ -4,5 +4,12 @@
     for ($i=1;$i<=$nbre;$i++) {
         $s+=$i;    
     }
+    echo"la somme des entiers entre 1 et $nbre = $s <br>";
+    $i=1;
+    $s=0;
+    while ($i<=$nbre) {
+        $s+=$i;
+        $i++;  
+    }
     echo"la somme des entiers entre 1 et $nbre = $s";
 ?>
